760b6bb784b1a9db006c2ff534ebeedf1a02b8c1
olegfkl <olegfkl@gmail.com>
2018-09-16 16:52:50 +0100

    Deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,19 +1,27 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const {interface, bytecode} = require('../compile.js');
 
-
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
 
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hello ETH World']})
+        .send({from: accounts[0], gas: '1000000'});
+
+    inbox.setProvider(provider);
+
 });
 
 describe('Inbox', () => {
-    it('should deploy a contract ',  () => {
-        console.log(accounts);
+    it('should deploy a contract ', () => {
+        assert.ok(inbox.options.address);
     });
 });
