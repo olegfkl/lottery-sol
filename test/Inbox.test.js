380326b24ec887dfa6115e0088871d7a0e0660bc
olegfkl <olegfkl@gmail.com>
2018-09-16 17:13:03 +0100

    unit test the methods of contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,6 +6,7 @@ const {interface, bytecode} = require('../compile.js');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
 
+const INITIAL_STRING = 'Hello ETH World';
 let accounts;
 let inbox;
 
@@ -13,15 +14,31 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hello ETH World']})
-        .send({from: accounts[0], gas: '1000000'});
+        .deploy({
+            data: bytecode, arguments: [INITIAL_STRING]
+        })
+        .send({
+            from: accounts[0], gas: '1000000'
+        });
 
     inbox.setProvider(provider);
-
 });
 
 describe('Inbox', () => {
     it('should deploy a contract ', () => {
         assert.ok(inbox.options.address);
+    });
+
+    it('should have a default message ', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    });
+
+    it('should set a new message ', async () => {
+        await inbox.methods.setMessage('new message').send({
+            from: accounts[0]
+        });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'new message');
     });
 });
